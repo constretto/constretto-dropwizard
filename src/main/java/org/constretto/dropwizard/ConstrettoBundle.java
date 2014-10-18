8bd3b252bcf7584f6e5b74d25fa479fc58892de7
Kjetil Iversen <kjeivers@gmail.com>
2014-10-18 22:04:19 +0200

    Do not need it to be a ConfiguredBundle

--- src/main/java/org/constretto/dropwizard/ConstrettoBundle.java
+++ src/main/java/org/constretto/dropwizard/ConstrettoBundle.java
@@ -1,5 +1,6 @@
 package org.constretto.dropwizard;
 
+import io.dropwizard.Bundle;
 import io.dropwizard.Configuration;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
@@ -11,14 +12,25 @@ import org.constretto.resolver.ConfigurationContextResolver;
  *
  * @author kjeivers
  */
-public class ConstrettoBundle<T extends Configuration> implements io.dropwizard.ConfiguredBundle<T> {
+public class ConstrettoBundle<T extends Configuration> implements Bundle {
 
     private final ConfigurationContextResolver tagResolver;
 
+    /**
+     * Constretto-support using the DefaultConfigurationContextResolver.
+     * This means that the constretto tags is read from the system property 'CONSTRETTO_TAGS'
+     * (or the environment variable with the same name)
+     *
+     * -DCONSTRETTO_TAGS=a,b,c,d
+     */
     public ConstrettoBundle() {
         this(new DefaultConfigurationContextResolver());
     }
 
+    /**
+     * Constretto-support allowing an arbitrary tag resolver
+     * @param tagResolver Provider of the set of active constretto tags
+     */
     public ConstrettoBundle(ConfigurationContextResolver tagResolver) {
         this.tagResolver = tagResolver;
     }
@@ -29,13 +41,14 @@ public class ConstrettoBundle<T extends Configuration> implements io.dropwizard.
      * @param bootstrap Provides the original ConfigurationSourceProvider
      */
     @Override
+    @SuppressWarnings("unchecked")
     public void initialize(Bootstrap bootstrap) {
         bootstrap.setConfigurationSourceProvider(new ConstrettoConfigurationProvider(bootstrap.getConfigurationSourceProvider()));
         bootstrap.setConfigurationFactoryFactory(new ConstrettoConfigurationFactoryFactory<T>(tagResolver));
     }
 
     @Override
-    public void run(T configuration, Environment environment) throws Exception {
+    public void run(Environment environment) {
         // all work is done in initialize()
     }
 
